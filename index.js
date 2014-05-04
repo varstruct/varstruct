40050098bba03b7d61ea1761fdcb7523852116e3
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-05 00:14:41 +0200

    varbuffers with variable sized lengths!

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+var varint = require('varint')
 
+var ONE = 128, TWO = 128*128, THREE = 128*128*128, FOUR = 128*128*128*128
+
+//I'll make this into a pull request for varint later
+//if this turns out to be a good idea.
+
+varint.dynamicLength = function (v) {
+  console.log('varint.dynamicLength', v)
+  return (
+    v <= ONE   ? 1
+  : v <= TWO   ? 2
+  : v <= THREE ? 3
+  : v <= FOUR  ? 4
+  :              5
+  )
+}
 
 function reduce(obj, iter, acc) {
   for(var k in obj)
@@ -8,19 +24,29 @@ function reduce(obj, iter, acc) {
 
 
 exports = module.exports = function (parts) {
+  var funLen = false
   var length = reduce(parts, function (acc, v) {
-    console.log(acc, v)
-    if(isNaN(v.length)) throw new Error('length is not a number')
+    if(isNaN(v.length)) return funLen = true
     return acc + v.length
   }, 0)
 
+  function lengthOf(part, value) {
+    return part.length || part.dynamicLength(value)
+  }
+
+  function getLength(obj) {
+    return reduce(parts, function (acc, part, k) {
+      return acc + lengthOf(part, obj[k])
+    }, 0)
+  }
+
   return {
     encode: function (obj) {
-      var b = new Buffer(length)
+      var b = new Buffer(funLen ? getLength(obj) : length )
       var offset = 0
       for(var k in parts) {
         parts[k].encode(obj[k], b, offset)
-        offset += parts[k].length
+        offset += lengthOf(parts[k], obj[k])
       }
       return b
     },
@@ -29,11 +55,12 @@ exports = module.exports = function (parts) {
       var offset = 0
       for(var k in parts) {
         obj[k] = parts[k].decode(buffer, offset)
-        offset += parts[k].length | 0
+        offset += lengthOf(parts[k], obj[k]) | 0
       }
       return obj
     },
-    length: length
+    length: funLen ? null : length,
+    dynamicLength: getLength
   }
 }
 
@@ -50,17 +77,26 @@ exports.int8 = {
   length: 1
 }
 
-exports.doubleBE = {
-  encode: function (value, b, offset) {
-    b = b || new Buffer(8)
-    b.writeDoubleBE(value, offset | 0)
-    return b
-  },
-  decode: function (buffer, offset) {
-    return buffer.readDoubleBE(offset|0)
-  },
-  length: 8
+function createNumber(type, len) {
+  var read = Buffer.prototype['read' + type]
+  var write = Buffer.prototype['write' + type]
+  return {
+    encode: function (value, b, offset) {
+      b = b || new Buffer(len)
+      write.call(b, value, offset | 0)
+      return b
+    },
+    decode: function (buffer, offset) {
+      return read.call(buffer, offset|0)
+    },
+    length: len
+  }
 }
+
+exports.UInt32BE = createNumber('UInt32BE', 4)
+
+exports.doubleBE =
+exports.DoubleBE = createNumber('DoubleBE', 8)
 
 exports.array = function (len) {
   return {
@@ -80,23 +116,25 @@ exports.array = function (len) {
 exports.varbuf = function (lenType) {
   return {
     encode: function (value, buffer, offset) {
-    buffer = buffer || new Buffer(this.length(value))
+    buffer = buffer || new Buffer(this.dynamicLength(value) )
       offset = offset | 0
       buffer = lenType.encode(value.length, buffer, offset)
-      console.log(buffer, this.length(value), lenType)
-      offset += lenType.length
+      offset += lenType.length || lenType.dynamicLength(value.length)
       value.copy(buffer, offset, 0, value.length)
       return buffer
     },
     decode: function (buffer, offset) {
       offset = offset | 0
       var length = lenType.decode(buffer, offset)
-      offset += lenType.length
+      offset += lenType.length || lenType.dynamicLength(length)
       return buffer.slice(offset, offset + length)
     },
-    length: function (value) {
-      return value.length + lenType.length
+    dynamicLength: function (value) {
+      return value.length + (lenType.length || lenType.dynamicLength(value.length))
     }
 
   }
 }
+
+
+exports.varint = varint
