8c152190987d6a785f0efb2901c1e1adcf0b507b
Kirill Fomichev <fanatid@ya.ru>
2016-03-02 17:13:22 +0300

    add tests with bitcoin transactions

--- test/index.js
+++ test/index.js
@@ -45,3 +45,60 @@ tap.test('length', function (t) {
 
   t.end()
 })
+
+tap.test('bitcoin transactions', function (t) {
+  var VarUIntBitcoin = require('varuint-bitcoin')
+  var TxInput = varstruct([
+    { name: 'hash', type: varstruct.Buffer(32) },
+    { name: 'index', type: varstruct.UInt32LE },
+    { name: 'script', type: varstruct.VarBuffer(VarUIntBitcoin) },
+    { name: 'sequence', type: varstruct.UInt32LE }
+  ])
+  var TxOutput = varstruct([
+    { name: 'value', type: varstruct.UInt64LE },
+    { name: 'script', type: varstruct.VarBuffer(VarUIntBitcoin) }
+  ])
+  var Tx = varstruct([
+    { name: 'version', type: varstruct.UInt32LE },
+    { name: 'ins', type: varstruct.VarArray(VarUIntBitcoin, TxInput) },
+    { name: 'outs', type: varstruct.VarArray(VarUIntBitcoin, TxOutput) },
+    { name: 'locktime', type: varstruct.UInt32LE }
+  ])
+
+  function buffer2hex (obj) {
+    for (var k in obj) {
+      if (Buffer.isBuffer(obj[k])) obj[k] = obj[k].toString('hex')
+      else if (Array.isArray(obj[k])) obj[k] = obj[k].map(buffer2hex)
+      else if (typeof obj[k] === 'object') obj[k] = buffer2hex(obj[k])
+    }
+    return obj
+  }
+
+  function isHex (s) {
+    return s.length % 2 === 0 && /^[0-9a-f]*$/.test(s)
+  }
+
+  function hex2buffer (obj) {
+    for (var k in obj) {
+      if (Array.isArray(obj[k])) obj[k] = obj[k].map(hex2buffer)
+      else if (typeof obj[k] === 'string' && isHex(obj[k])) obj[k] = new Buffer(obj[k], 'hex')
+    }
+    return obj
+  }
+
+  require('./fixtures/bitcoin-tx.json').forEach(function (fixture) {
+    t.test('encode ' + fixture.description, function (t) {
+      var result = Tx.encode(hex2buffer(fixture.raw))
+      t.same(result.toString('hex'), fixture.hex)
+      t.end()
+    })
+
+    t.test('decode ' + fixture.description, function (t) {
+      var result = Tx.decode(new Buffer(fixture.hex, 'hex'))
+      t.same(buffer2hex(result), buffer2hex(fixture.raw))
+      t.end()
+    })
+  })
+
+  t.end()
+})
